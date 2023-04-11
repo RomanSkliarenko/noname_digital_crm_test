49799832d37e4f5e36d20f1c0abcb2a630dba8e4
Roman Skliarenko <vurtnevk@gmail.com>
2023-04-11 17:33:27 +0300

    Updates

--- src/components/private-route/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import {Navigate} from 'react-router-dom';
-
-const PrivateRoute = ({ component, user }) => {
-    return user ? component : <Navigate to={'/login'} />;
-};
-
-export default PrivateRoute;
--- src/components/public-route/PublicRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const PublicRoute = ({ component, user }) => {
-	return user ? <Navigate to={'/'} /> : component;
-};
-
-export default PublicRoute;
